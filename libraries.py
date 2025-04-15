9c52ad4de9541405d93cd37e9e330823761beb72
Olive Njoroge <njorogeolive001@gmail.com>
2025-04-15 23:03:09 +0300

    Answered question 2

--- libraries.py
+++ libraries.py
@@ -4,3 +4,15 @@ import numpy as np
 my_array = np.array([1,2,3,4,5,6,7,8,9,10])
 mean = np.mean(my_array)
 print(f"Mean: {mean}")
+
+#Load a small dataset into a pandas DataFrame and display summary statistics.
+data = {
+    'Name': ["Olive", "Waigumo", "Njoroge", "Jane"],
+    'Age': [1,2,3,4],
+    'Height': [120,110,125,130]
+}
+
+person = pd.DataFrame(data)
+print(person)
+#summary statistics
+print(person.describe())
