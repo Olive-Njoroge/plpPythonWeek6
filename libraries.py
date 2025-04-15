ee403cefc778afed50c5699f52d5f7cb6a84dd1c
Olive Njoroge <njorogeolive001@gmail.com>
2025-04-15 23:06:43 +0300

    Answered question 3

--- libraries.py
+++ libraries.py
@@ -6,6 +6,7 @@ mean = np.mean(my_array)
 print(f"Mean: {mean}")
 
 #Load a small dataset into a pandas DataFrame and display summary statistics.
+imprt pandas as pd
 data = {
     'Name': ["Olive", "Waigumo", "Njoroge", "Jane"],
     'Age': [1,2,3,4],
@@ -16,3 +17,18 @@ person = pd.DataFrame(data)
 print(person)
 #summary statistics
 print(person.describe())
+
+#Fetch data from a public API using requests and print a key piece of information.
+import requests
+
+# API URL
+url = "https://catfact.ninja/fact"
+
+# Make the request
+response = requests.get(url)
+
+# Convert response to JSON
+data = response.json()
+
+# Print a key piece of information
+print("Random Cat Fact:", data['fact'])
