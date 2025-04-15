cc613d18082f6e0e8d87b7632e1ef3d536bcdb88
Olive Njoroge <njorogeolive001@gmail.com>
2025-04-15 22:48:47 +0300

    Answered the first question

---
+++ libraries.py
@@ -0,0 +1,6 @@
+#Create a NumPy array of numbers from 1 to 10 and calculate the mean.
+import numpy as np
+
+my_array = np.array([1,2,3,4,5,6,7,8,9,10])
+mean = np.mean(my_array)
+print(f"Mean: {mean}")
